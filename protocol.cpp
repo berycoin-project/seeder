706cfdfe0244fe5b74e96d8e8f5a1a8b2e5f0f19
root <root@vmi144710.contaboserver.net>
2017-10-27 08:30:56 +0200

    First commit

--- protocol.cpp
+++ protocol.cpp
@@ -22,7 +22,7 @@ static const char* ppszTypeName[] =
     "block",
 };
 
-unsigned char pchMessageStart[4] = { 0xfb, 0xc0, 0xb6, 0xdb };
+unsigned char pchMessageStart[4] = { 0x90, 0x8E, 0x8C, 0x8A };
 
 CMessageHeader::CMessageHeader()
 {
