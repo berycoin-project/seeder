d3f5de6d0c74c25e570eaee84a05db123a625aad
berycoin-project <root@vmi146407.contaboserver.net>
2018-01-29 02:52:07 +0100

    changes

--- protocol.h
+++ protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 19944 : 9944;
+    return testnet ? 19761 : 9761;
 }
 
 //
